dcaa62d9fb214270c0d3fe90283947a80fda4aa3
Sergey Lebedev <oddyone@mail.ru>
2018-09-15 23:03:56 +0300

    Add project

---
+++ index.js
@@ -0,0 +1,63 @@
+const http = new easyHTTP;
+
+// Create Data
+const someData = {
+  title: 'Custom Post',
+  body: 'This Custom Post'
+};
+
+
+// Get Request
+// (function() {
+// // Get Posts
+// http.get('https://jsonplaceholder.typicode.com/posts',
+//     function(err, posts) {
+//         if (err) {
+//             console.log(err)
+//         } else {
+//             console.log(posts);
+//         }
+//     });
+
+// Get Single Post
+// http.get('https://jsonplaceholder.typicode.com/posts/2',
+//     function(err, posts) {
+//         if (err) {
+//             console.log(err)
+//         } else {
+//             console.log(posts);
+//         }
+//     });
+// })();
+
+// Post Request
+
+// Create Post
+http.post('https://jsonplaceholder.typicode.com/posts', someData, function(err, post) {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log(post);
+    }
+});
+
+// Update Post
+// http.put('https://jsonplaceholder.typicode.com/posts/5',
+//     someData,
+//     function(err, post) {
+//         if (err) {
+//             console.log(err)
+//         } else {
+//             console.log(post);
+//         }
+//     });
+
+// Delete Post
+// http.delete('https://jsonplaceholder.typicode.com/posts/1',
+//     function(err, response) {
+//         if (err) {
+//             console.log(err)
+//         } else {
+//             console.log(response);
+//         }
+//     });
